0b771a3fd2e085b69499a754ef1967400885f60e
justinwols88 <justin.wold88@gmail.com>
2025-11-12 01:43:20 -0800

    Add initial migration script and update mechanic schemas

--- application/blueprints/mechanic/mechanicSchemas.py
+++ application/blueprints/mechanic/mechanicSchemas.py
@@ -1,18 +1,18 @@
 # application/schemas/mechanic_schema.py
 from marshmallow import Schema, fields
 class MechanicSchema(Schema):
-    id = fields.Int(dump_only=True)
-    name = fields.Str(required=True)
-    email = fields.Email(required=True)
-    password = fields.Str(required=True, load_only=True)
-    specialization = fields.Str(required=True)
-    phone = fields.Str()
-    address = fields.Str()
-    created_at = fields.DateTime(dump_only=True)
+        id = fields.Int(dump_only=True)
+        name = fields.Str(required=True)
+        email = fields.Email(required=True)
+        password = fields.Str(required=True, load_only=True)
+        specialization = fields.Str(required=True)
+        phone = fields.Str()
+        address = fields.Str()
+        created_at = fields.DateTime(dump_only=True)
 
 class LoginSchema(Schema):
-    email = fields.Email(required=True)
-    password = fields.Str(required=True)
+        email = fields.Email(required=True)
+        password = fields.Str(required=True)
 
 # Create instances
 mechanic_schema = MechanicSchema()
